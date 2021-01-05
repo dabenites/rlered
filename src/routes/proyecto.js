378c691ac2c62a2374b08820a7b7dbe9e2ab9921
frguerrerod <panchaguerrerodiaz@gmail.com>
2021-01-05 11:55:48 -0300

    proyectos
    cambio bd

--- src/routes/proyecto.js
+++ src/routes/proyecto.js
@@ -17,65 +17,67 @@ const { Console } = require('console');
 
 router.get('/iproyecto', isLoggedIn, async (req, res) => {
   // const proyectos = await pool.query("SELECT * FROM pro_proyectos as t1, proyecto_tipo as t2 WHERE t1.Tipo = t2.Descripcion");
-
+  
+  const estado = await pool.query("SELECT * FROM pro_costo_externo_estado");
   const usuarios = await pool.query("SELECT * FROM sys_usuario");
   const pais = await pool.query("SELECT * FROM pais");
   const tipo = await pool.query("SELECT * FROM proyecto_tipo");
   const categoria = await pool.query("SELECT * FROM categorias");
-  const proye = await pool.query("SELECT * FROM pro_proyectos");
+  const proye = await pool.query("SELECT * FROM pro_proyectos_copy");
   const contacto = await pool.query("SELECT * FROM contacto");
-  const proyectos = await pool.query("SELECT * FROM pro_proyectos as t1 ORDER BY year DESC, code DESC");
+  const proyectos = await pool.query("SELECT * FROM pro_proyectos_copy as t1 ORDER BY year DESC, code DESC");
 
 
-  res.render('proyecto/iproyecto', { req, usuarios, pais, tipo, categoria, proye, contacto, proyectos, layout: 'template' });
+  res.render('proyecto/iproyecto', { req, usuarios, pais, tipo, categoria, proye, contacto, proyectos,estado, layout: 'template' });
 });
 
 
 router.post('/addProyecto', async (req, res) => {
-  const { nombre, year, Tipo,/*Nproyecto*/Estado, Complejidad, SuperficiePPTO, SuperficieAPC, pais, Ciudad,
-    Ubicacion, Npisos, Nsubterraneo, DP, JP, ValorMC, Zona, Suelo, FechaIni, FechaEnt,
-    FechaTer, Cliente, Arquitectura, Constructora, Revisor, Nplanos, Servicio } = req.body; //Obtener datos title,url,description
+  const { nombre, year,code, id_Tipo,id_Estado, id_complejidad, SuperficiePPTO, SuperficieAPC, id_Pais, Ciudad,
+    Ubicacion, Npisos, Nsubterraneo, id_Director_Proyecto, id_Jefe_Proyecto, ValorMC, Zona, Suelo, FechaIni, FechaEnt,
+    FechaTer, id_Cliente, id_Arquitectura, id_Constructora, id_Revisor, Nplanos, id_Servicio } = req.body; //Obtener datos title,url,description
 
 
 
   const newProyecto = { //Se gurdaran en un nuevo objeto
-    //Nproyecto : Nproyecto, nombre : proyecto[0].year + "-" +proyecto[0].code + " " + proyecto[0].nombre,
+   // Nproyecto : Nproyecto, nombre : proyecto[0].year + "-" +proyecto[0].code + " " + proyecto[0].nombre,
     nombre: nombre,
     year: year,
-    Tipo: Tipo,
-    Estado: Estado,
-    Complejidad: Complejidad,
+    code:code,
+    id_Tipo: id_Tipo,
+    id_Estado: id_Estado,
+    id_complejidad: id_complejidad,
     SuperficiePPTO: SuperficiePPTO,
     SuperficieAPC: SuperficieAPC,
-    pais: pais,
+    id_Pais: id_Pais,
     Ciudad: Ciudad,
     Ubicacion: Ubicacion,
     Npisos: Npisos,
     Nsubterraneo: Nsubterraneo,
-    DP: DP,
-    JP: JP,
+    id_Director_Proyecto: id_Director_Proyecto,
+    id_Jefe_Proyecto: id_Jefe_Proyecto,
     ValorMC: ValorMC,
     Zona: Zona,
     Suelo: Suelo,
     FechaIni: FechaIni,
     FechaEnt: FechaEnt,
     FechaTer: FechaTer,
-    Cliente: Cliente,
-    Arquitectura: Arquitectura,
-    Constructora: Constructora,
-    Revisor: Revisor,
+    id_Cliente: id_Cliente,
+    id_Arquitectura: id_Arquitectura,
+    id_Constructora: id_Constructora,
+    id_Revisor: id_Revisor,
     Nplanos: Nplanos,
-    Servicio: Servicio
+    id_Servicio: id_Servicio
 
 
   };
   //Guardar datos en la BD     
   //console.log(req.body);
-  const result = await pool.query('INSERT INTO pro_proyectos set ?', [newProyecto]);//Inserción
+  const result = await pool.query('INSERT INTO pro_proyectos_copy set ?', [newProyecto]);//Inserción
 
 
   console.log(result);
-  const proye = await pool.query('SELECT * FROM pro_proyectos');
+  const proye = await pool.query('SELECT * FROM pro_proyectos_copy');
 
 
 
@@ -86,16 +88,18 @@ router.post('/addProyecto', async (req, res) => {
 
 
 
-//BUSCADOR
+//BUSCADOR inicial bd
 
 router.get('/buscador', isLoggedIn, async (req, res) => {
-  const buscadores = await pool.query("SELECT * FROM pro_proyectos");
+  const estado = await pool.query("SELECT * FROM pro_costo_externo_estado");
+  const buscadores = await pool.query("SELECT * FROM pro_proyectos_copy");
   const tipo = await pool.query("SELECT * FROM proyecto_tipo");
   const usuarios = await pool.query("SELECT * FROM sys_usuario");
   const contacto = await pool.query("SELECT * FROM contacto");
+  const paises = await pool.query("SELECT * FROM pais");
 
 
-  res.render('proyecto/buscador', { req, buscadores, tipo, usuarios, contacto, layout: 'template' });
+  res.render('proyecto/buscador', { req, buscadores, tipo, usuarios, contacto,estado,paises, layout: 'template' });
 });
 
 
@@ -157,6 +161,8 @@ router.get('/buscador', isLoggedIn, async (req, res) => {
 
 //LISTAR
 
+
+
 router.post('/listPro', isLoggedIn, async (req, res) => {
   console.log(req);
   // const buscadores = await pool.query("SELECT * FROM pro_proyectos");
@@ -167,96 +173,56 @@ router.post('/listPro', isLoggedIn, async (req, res) => {
 
   var a = req.body.nombre;
   var b = req.body.year;
-  var c = req.body.Tipo;
-  var d = req.body.Estado;
-  var e = req.body.Complejidad;
+  var c = req.body.id_Tipo;
+  var co = req.body.code;
+  /*var d = req.body.id_Estado;
+  var e = req.body.id_complejidad;
   var f = req.body.SuperficiePPTO;
   var g = req.body.SuperficieAPC;
-  var h = req.body.Pais;
+  var h = req.body.id_Pais;
   var i = req.body.Ciudad;
   var k = req.body.Npisos;
   var l = req.body.Nsubterraneo;
-  var m = req.body.DP;
-  var n = req.body.JP;
-  var o = req.body.ValorMC;
+  var m = req.body.id_Director_Proyecto;
+  var n = req.body.id_Jefe_Proyecto;
+  var o = req.body.ValorMC;*/
   var p = req.body.Zona;
   var q = req.body.Suelo;
-  var r = req.body.FechaIni;
+  /*var r = req.body.FechaIni;
   var s = req.body.FechaEnt;
   var t = req.body.FechaTer;
-  var u = req.body.Cliente;
-  var v = req.body.Arquitectura;
-  var w = req.body.Constructora;
-  var x = req.body.Revisor;
+  var u = req.body.id_Cliente;
+  var v = req.body.id_Arquitectura;
+  var w = req.body.id_Constructora;
+  var x = req.body.id_Revisor;
   var y = req.body.Nplanos;
-  var z = req.body.Servicio;
+  var z = req.body.id_Servicio;*/
 
 
-  const buscadores = await pool.query("SELECT * FROM pro_proyectos AS t1 " +
-    " WHERE " +
-    " t1.nombre=? OR t1.year=? OR t1.Tipo=? OR t1.Estado=? OR t1.Complejidad=? OR t1.SuperficiePPTO=? OR t1.SuperficieAPC=? OR t1.Pais=? OR t1.Ciudad=?  OR t1.Npisos=? OR t1.Nsubterraneo=? OR t1.DP=? OR t1.JP=? OR t1.ValorMC=? OR t1.Zona=? OR t1.Suelo=? OR t1.FechaIni=? OR t1.FechaEnt=? OR t1.FechaTer=? OR t1.Cliente=? OR t1.Arquitectura=? OR t1.Constructora=? OR t1.Revisor=? OR t1.Nplanos=?  OR t1.Servicio=?", [a, b, c, d, e, f, g, h, i, k, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]);
+  //const buscadores = await pool.query("SELECT * FROM pro_proyectos_copy AS t1 " +
+    //" WHERE " +
+    //" t1.nombre=? OR t1.year=? OR t1.id_Tipo=? OR t1.id_Estado=? OR t1.id_complejidad=? OR t1.SuperficiePPTO=? OR t1.SuperficieAPC=? OR t1.id_Pais=? OR t1.Ciudad=?  OR t1.Npisos=? OR t1.Nsubterraneo=? OR t1.id_Director_Proyecto=? OR t1.id_Jefe_Proyecto=? OR t1.ValorMC=? OR t1.Zona=? OR t1.Suelo=? OR t1.FechaIni=? OR t1.FechaEnt=? OR t1.FechaTer=? OR t1.id_Cliente=? OR t1.id_Arquitectura=? OR t1.id_Constructora=? OR t1.id_Revisor=? OR t1.Nplanos=?  OR t1.id_Servicio=?", [a, b, c, d, e, f, g, h, i, k, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]);
 
 
   // console.log(a + "///" + b);
+
+
+  const buscadores = await pool.query("SELECT * FROM pro_proyectos_copy AS t1 " +
+    " WHERE " +
+    " t1.nombre=? OR t1.year=? OR t1.id_Tipo=? OR t1.code=? OR t1.Zona=? OR t1.Zona=? ", [a,b,c,co,p,q]);
+
 
 
   res.render('proyecto/listPro', { req, buscadores, layout: 'template' });
 });
 
 
-router.post('/editBusca', async (req, res) => {
-  const { id, nombre, year, Tipo, Estado, Complejidad, SuperficiePPTO, SuperficieAPC, Pais, Ciudad,
-    Ubicacion, Npisos, Nsubterraneo, DP, JP, ValorMC, Zona, Suelo, FechaIni, FechaEnt,
-    FechaTer, Cliente, Arquitectura, Constructora, Revisor, Nplanos, Servicio } = req.body; //Obtener datos title,url,description
-
-  const newBuscador = { //Se gurdaran en un nuevo objeto
-    //Nproyecto : Nproyecto, nombre : proyecto[0].year + "-" +proyecto[0].code + " " + proyecto[0].nombre,
-    nombre: nombre,
-    year: year,
-    Tipo: Tipo,
-    Estado: Estado,
-    Complejidad: Complejidad,
-    SuperficiePPTO: SuperficiePPTO,
-    SuperficieAPC: SuperficieAPC,
-    Pais: Pais,
-    Ciudad: Ciudad,
-    Ubicacion: Ubicacion,
-    Npisos: Npisos,
-    Nsubterraneo: Nsubterraneo,
-    DP: DP,
-    JP: JP,
-    ValorMC: ValorMC,
-    Zona: Zona,
-    Suelo: Suelo,
-    FechaIni: FechaIni,
-    FechaEnt: FechaEnt,
-    FechaTer: FechaTer,
-    Cliente: Cliente,
-    Arquitectura: Arquitectura,
-    Constructora: Constructora,
-    Revisor: Revisor,
-    Nplanos: Nplanos,
-    Servicio: Servicio
-  };
-  //Guardar datos en la BD     
-  await pool.query('UPDATE pro_proyectos set ? WHERE id = ?', [newBuscador, id]);
-  res.redirect('../proyecto/listPro');
-
-});
-
 
 router.get('/buscador/edit/:id', async (req, res) => {
   const { id } = req.params;
 
-  const buscadores = await pool.query("SELECT * FROM pro_proyectos ");
-  const buscador = await pool.query("SELECT * FROM pro_proyectos as t1 WHERE t1.id = ?", [id]);
-
-  //const isEqualHelperHandlerbar = function (a, b, opts) {
-    //if (a == b) {
-      //return true
-    ////return false
-    //}
-  //};
+  const buscadores = await pool.query("SELECT * FROM pro_proyectos_copy ");
+  const buscador = await pool.query("SELECT * FROM pro_proyectos_copy as t1 WHERE t1.id = ?", [id]);
 
 
 
@@ -267,33 +233,110 @@ router.get('/buscador/edit/:id', async (req, res) => {
   });
 });
 
-/*router.post('/buscador/edit/:id', async (req, res) => {
-  const { id } = req.params;
 
-  const buscadores = await pool.query("SELECT * FROM pro_proyectos ");
-  const buscador = await pool.query("SELECT * FROM pro_proyectos as t1 WHERE t1.id = ?", [id]);
 
-  const isEqualHelperHandlerbar = function(a, b, opts) {
-    if (a == b) {
-        return true
-    } else { 
-        return false
-    } 
-};
-console.log(usuario);
+router.post('/editBusca', async (req, res) => {
+  const { id,nombre, year,code, id_Tipo,id_Estado, id_complejidad, SuperficiePPTO, SuperficieAPC, id_Pais, Ciudad,
+    Ubicacion, Npisos, Nsubterraneo, id_Director_Proyecto, id_Jefe_Proyecto, ValorMC, Zona, Suelo, FechaIni, FechaEnt,
+    FechaTer, id_Cliente, id_Arquitectura, id_Constructora, id_Revisor, Nplanos, id_Servicio } = req.body; //Obtener datos title,url,description
 
-res.send("sadads");
+  const newBuscador = { //Se gurdaran en un nuevo objeto
+  
+    
+    nombre: nombre,
+    year: year,
+    code:code,
+    id_Tipo: id_Tipo,
+    id_Estado: id_Estado,
+    id_complejidad: id_complejidad,
+    SuperficiePPTO: SuperficiePPTO,
+    SuperficieAPC: SuperficieAPC,
+    id_Pais: id_Pais,
+    Ciudad: Ciudad,
+    Ubicacion: Ubicacion,
+    Npisos: Npisos,
+    Nsubterraneo: Nsubterraneo,
+    id_Director_Proyecto: id_Director_Proyecto,
+    id_Jefe_Proyecto: id_Jefe_Proyecto,
+    ValorMC: ValorMC,
+    Zona: Zona,
+    Suelo: Suelo,
+    FechaIni: FechaIni,
+    FechaEnt: FechaEnt,
+    FechaTer: FechaTer,
+    id_Cliente: id_Cliente,
+    id_Arquitectura: id_Arquitectura,
+    id_Constructora: id_Constructora,
+    id_Revisor: id_Revisor,
+    Nplanos: Nplanos,
+    id_Servicio: id_Servicio
+  };
+  //Guardar datos en la BD     
+  await pool.query('UPDATE pro_proyectos_copy set ? WHERE id = ?', [newBuscador, id]);
+  res.redirect('/proyecto/editProyecto');
 
-  //res.render('proyecto/editProyecto', { buscadores, buscador: buscador[0], req, layout: 'template', helpers : {
-  //  if_equal : isEqualHelperHandlerbar }});
-});*/
+});
 
+
+router.post('/edit', async (req, res) => {
+  const { id,nombre, year,code, id_Tipo,id_Estado, id_complejidad, SuperficiePPTO, SuperficieAPC, id_Pais, Ciudad,
+    Ubicacion, Npisos, Nsubterraneo, id_Director_Proyecto, id_Jefe_Proyecto, ValorMC, Zona, Suelo, FechaIni, FechaEnt,
+    FechaTer, id_Cliente, id_Arquitectura, id_Constructora, id_Revisor, Nplanos, id_Servicio } = req.body; //Obtener datos title,url,description
+
+  const newBuscador = { //Se gurdaran en un nuevo objeto
+  
+    id:id,
+    nombre: nombre,
+    year: year,
+    code:code,
+    id_Tipo: id_Tipo,
+    id_Estado: id_Estado,
+    id_complejidad: id_complejidad,
+    SuperficiePPTO: SuperficiePPTO,
+    SuperficieAPC: SuperficieAPC,
+    id_Pais: id_Pais,
+    Ciudad: Ciudad,
+    Ubicacion: Ubicacion,
+    Npisos: Npisos,
+    Nsubterraneo: Nsubterraneo,
+    id_Director_Proyecto: id_Director_Proyecto,
+    id_Jefe_Proyecto: id_Jefe_Proyecto,
+    ValorMC: ValorMC,
+    Zona: Zona,
+    Suelo: Suelo,
+    FechaIni: FechaIni,
+    FechaEnt: FechaEnt,
+    FechaTer: FechaTer,
+    id_Cliente: id_Cliente,
+    id_Arquitectura: id_Arquitectura,
+    id_Constructora: id_Constructora,
+    id_Revisor: id_Revisor,
+    Nplanos: Nplanos,
+    id_Servicio: id_Servicio
+  };
+  //Guardar datos en la BD     
+  //await pool.query('UPDATE pro_proyectos_copy set nombre =  ? WHERE id = ?', [nombre, id]);
+  //res.redirect('./proyecto/listPro');
+
+  //res.send("ppp");
+
+//console.log(nombre+"ind"+id);
+
+const Pedit = await pool.query('UPDATE pro_proyectos_copy set nombre =  ? , code =  ?,  SuperficiePPTO =  ?,  SuperficieAPC =  ?, Ciudad =  ?, Ubicacion =  ?,Npisos =  ?,Nsubterraneo =  ?,ValorMC =  ?,Zona =  ?,Suelo =  ?,Nplanos = ? WHERE id = ?', [nombre,code,SuperficiePPTO,SuperficieAPC,Ciudad,Ubicacion,Npisos, Nsubterraneo,ValorMC,Zona,Suelo,Nplanos,id] );
+ // console.log(nombre);
+  //res.send("ppp");
+
+ //const ep = await pool.query('SELECT * FROM pro_proyectos_copy');
+
+  res.redirect('/proyecto/buscador');
+
+});
 
 
 
 router.get('/buscador/delete/:id', async (req, res) => {
   const { id } = req.params;
-  await pool.query('DELETE FROM pro_proyectos WHERE id = ?', [id]);
+  await pool.query('DELETE FROM pro_proyectos_copy WHERE id = ?', [id]);
 
 
   res.redirect('/proyecto/buscador');
